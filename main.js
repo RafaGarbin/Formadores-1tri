aec8b101a577e819d22e37cabf430388a9ade63b
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2024-04-04 19:58:01 -0300

    ok

--- main.js
+++ main.js
@@ -26,4 +26,4 @@ let dias=Math.floor (horas/24);
 segundos %=60;
 minutos %=60;
 horas %=24
-return dias+" dias "+ horas+" horas "+minutos+" minutos "+segundos+" segundos ";
+return dias+" dias "+ horas+" horas "+minutos+" minutos "+segundos+" segundos ";}
