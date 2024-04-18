ea833c51f88336b25f3eca59596dfa65c22fd5bd
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2024-04-18 19:17:25 -0300

    ok

--- main.js
+++ main.js
@@ -17,7 +17,11 @@ const tempoObjetivo2 = new Date("2024-09-25T23:59:59");
 const tempoObjetivo3 = new Date("2024-08-01T23:59:59");
 const tempoObjetivo4 = new Date("2024-07-01T23:59:59");
 
-const tempos=[tempoObjetivo1, tempoObjetivo2, tempoObjetivo3,tempoObjetivo4]
+const tempos=[tempoObjetivo1, tempoObjetivo2, tempoObjetivo3,tempoObjetivo4];
+for (let i=0; i < contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);  
+}
+
 contadores[0].textContent = calculaTempo (tempoObjetivo1);
 
 function calculaTempo (tempoObjetivo) {
