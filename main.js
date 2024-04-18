319ba1492dfedccaeb381f004d94c10e20b9e319
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2024-04-18 19:05:00 -0300

    ok

--- main.js
+++ main.js
@@ -12,10 +12,12 @@ for(let i=0; i<botoes.length; i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-13T00:00:00");
+const tempoObjetivo1 = new Date("2024-12-13T23:59:59");
 const tempoObjetivo2 = new Date("2024-09-25T23:59:59");
 const tempoObjetivo3 = new Date("2024-08-01T23:59:59");
 const tempoObjetivo4 = new Date("2024-07-01T23:59:59");
+
+const tempos=[tempoObjetivo1, tempoObjetivo2, tempoObjetivo3,tempoObjetivo4]
 contadores[0].textContent = calculaTempo (tempoObjetivo1);
 
 function calculaTempo (tempoObjetivo) {
