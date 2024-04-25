955f56ed98e944e4e9a0f3de590d0feae2217ee2
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2024-04-25 19:15:17 -0300

    dd

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ segundos %=60;
 minutos %=60;
 horas %=24
 if (tempoFinal>0){
-    return (dias+" dias "+ horas+" horas "+minutos+" minutos "+segundos+" segundos ");}
+    return dias+" dias "+ horas+" horas "+minutos+" minutos "+segundos+" segundos ";}
 }
 else {
     return "Prazo finalizado";
